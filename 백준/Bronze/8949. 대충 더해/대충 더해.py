8db4c8bbc6260e77c7297a72d202da969108148e
youn yoo tak <53210731+yoon-yoo-tak@users.noreply.github.com>
2022-05-16 15:15:40 +0900

    [Bronze II] Title: 대충 더해, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/8949. 대충 더해/대충 더해.py
+++ 백준/Bronze/8949. 대충 더해/대충 더해.py
@@ -7,17 +7,13 @@ import sys
 
 input = sys.stdin.readline
 
-a, b = input().split()
-a = a[::-1]
-b = b[::-1]
-ans = []
+a, b = list(input().split())
+len_diff = abs(len(a) - len(b))
 if len(a) >= len(b):
-    for i in range(len(b)):
-        ans.append(str(int(a[i]) + int(b[i])))
-    ans += a[len(b):]
+    b = '0' * len_diff + b
 else:
-    for i in range(len(a)):
-        ans.append(str(int(a[i]) + int(b[i])))
-    ans += b[len(a):]
-ans.reverse()
-print(''.join(ans))
+    a = '0' * len_diff + a
+ans = ''
+for i in range(len(a)):
+    ans += str(int(a[i]) + int(b[i]))
+print(ans)
