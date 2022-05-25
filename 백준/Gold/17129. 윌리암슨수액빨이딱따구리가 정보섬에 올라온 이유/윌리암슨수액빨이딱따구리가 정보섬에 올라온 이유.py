ab8cbec101b18a54ec144471712cfdffc07545f8
youn yoo tak <53210731+yoon-yoo-tak@users.noreply.github.com>
2022-05-26 00:05:44 +0900

    [Gold V] Title: 윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유, Time: 1780 ms, Memory: 244096 KB -BaekjoonHub

--- 백준/Gold/17129. 윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유/윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유.py
+++ 백준/Gold/17129. 윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유/윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유.py
@@ -1,9 +1,5 @@
 """
 17129 윌리암슨수액빨이딱따구리가 정보섬에 올라온 이유
-
-1. 아이디어
-2. 시간복잡도
-3. 자료구조
 """
 
 import sys
@@ -34,7 +30,7 @@ def bfs(x, y):
             if a[nx][ny] in ('3', '4', '5'):
                 print('TAK')
                 print(dist[nx][ny])
-                exit()
+                return
     print('NIE')
 
 for i in range(n):
