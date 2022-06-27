7efa22ce597b1849c2df44475efd358514069020
youn yoo tak <53210731+yoon-yoo-tak@users.noreply.github.com>
2022-06-27 15:16:59 +0900

    [Silver III] Title: 두 수의 합, Time: 152 ms, Memory: 128152 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,27 +1,30 @@
+"""
+10816 두 수의 합
+"""
 import sys
-input=sys.stdin.readline
 
+input = sys.stdin.readline
 
-def bisect(a, l, r, x):
+n = int(input())
+ls = sorted(map(int, input().split()))
+x = int(input())
+
+ans = 0
+
+def bisect(a, l, r, target):
     while l <= r:
         mid = (l + r) // 2
-        if a[mid] == x:
+        if a[mid] == target:
             return True
-        if a[mid] < x:
+        if a[mid] < target:
             l = mid + 1
         else:
             r = mid - 1
     return False
 
 
-ans = 0
-n = int(input())
-a = sorted(map(int, input().split()))
-x = int(input())
-
 for i in range(n):
-    target = x - a[i]
-    if bisect(a, i + 1, n - 1, target):
+    if bisect(ls, i + 1, n - 1, x - ls[i]):
         ans += 1
 
 print(ans)
