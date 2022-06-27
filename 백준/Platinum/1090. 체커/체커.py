f1438e078deecfeee1781ba12741fa069bddbc82
youn yoo tak <53210731+yoon-yoo-tak@users.noreply.github.com>
2022-06-27 22:39:48 +0900

    [Platinum IV] Title: 체커, Time: 156 ms, Memory: 115484 KB -BaekjoonHub

--- 백준/Platinum/1090. 체커/체커.py
+++ 백준/Platinum/1090. 체커/체커.py
@@ -1,12 +1,16 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 ls = [list(map(int, input().split())) for _ in range(n)]
-a = [int(1e10) for _ in range(n + 1)]
+
+ans = [int(1e10) for _ in range(n + 1)]
+
 for i in range(n):
     for j in range(n):
-        x, y = ls[i][0], ls[j][1]
-        dist = sorted([abs(x - ls[k][0]) + abs(y - ls[k][1]) for k in range(n)])
-        total = 0
+        dist = sorted([abs(ls[i][0] - ls[k][0]) + abs(ls[j][1] - ls[k][1]) for k in range(n)])
+        temp = 0
         for k in range(n):
-            total += dist[k]
-            a[k + 1] = min(a[k + 1], total)
-print(*a[1:])
+            temp += dist[k]
+            ans[k + 1] = min(ans[k + 1], temp)
+print(*ans[1:])
