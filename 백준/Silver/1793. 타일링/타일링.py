4963e12ff2a92307e5756ae91214231a3217fac1
youn yoo tak <53210731+yoon-yoo-tak@users.noreply.github.com>
2022-06-28 13:59:46 +0900

    [Silver II] Title: 타일링, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/1793. 타일링/타일링.py
@@ -0,0 +1,16 @@
+"""
+타일링
+"""
+import sys
+input = sys.stdin.readline
+dp = [0] * 251
+dp[0] = 1
+dp[1] = 1
+dp[2] = 3
+for i in range(3, 251):
+    dp[i] = dp[i - 1] + (dp[i - 2] * 2)
+while True:
+    try:
+        print(dp[int(input())])
+    except:
+        break
