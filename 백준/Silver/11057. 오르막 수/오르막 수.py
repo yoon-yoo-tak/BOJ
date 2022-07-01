1279f03c31b924c26e5e59058f5ae34b373808c0
youn yoo tak <53210731+yoon-yoo-tak@users.noreply.github.com>
2022-07-01 12:38:39 +0900

    [Silver I] Title: 오르막 수, Time: 120 ms, Memory: 114488 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.py
@@ -0,0 +1,17 @@
+import sys, math
+input = sys.stdin.readline
+
+
+n = int(input())
+dp = [[0] * 10 for _ in range(n + 1)]
+
+for i in range(10):
+    dp[1][i] = 1
+
+for i in range(2, n + 1):
+    for j in range(10):
+        for prev in range(j + 1):
+            dp[i][j] += dp[i - 1][prev]
+            dp[i][j] %= 10007
+
+print(sum(dp[n]) % 10007)
